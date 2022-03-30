9644e81bb083e1d2ed3c48688360da81226c85cb
KristinBenson <kristin.benson@kcl.ac.uk>
2022-03-29 23:17:46 -0400

    add click functionality to like buttons

--- client/src/react-components/Comment/index.js
+++ client/src/react-components/Comment/index.js
@@ -19,42 +19,74 @@ function Comment(props) {
     }
 
     function getProfilePicture() {
-        console.log(props.comment.userId)
-        // const user = userListContext.getUserById(props.comment.userId);
-        // return user.profilePicture;
+        const user = userListContext.getUserById(props.comment.userId);
+        return user.profilePicture;
     }
 
     function likeComment(){
-        console.log(currentUser)
-        // commentContext.likeCommentByIds(props.comment.commentId);
+        vote(true)
     }
+
+    function dislikeComment (){
+        vote(false)
+    }
+
+    function vote(clickedLike){
+        if (currentUser.userId == null){
+            alert("You must be logged in to vote");
+            return
+        }
+
+        const likeIcon = document.getElementById("likeIcon" + props.comment.commentId);
+        const likeNum = document.getElementById("likeNum" + props.comment.commentId);
+        const dislikeIcon = document.getElementById("dislikeIcon" + props.comment.commentId);
+        const dislikeNum = document.getElementById("dislikeNum" + props.comment.commentId);
+
+        if (clickedLike){
+            likeIcon.style.color = "deepSkyBlue"
+            dislikeIcon.style.color = "black"
+            commentContext.likeCommentByIds(props.comment.commentId, currentUser.userId)
+        }
+        else {
+            dislikeIcon.style.color = "deepSkyBlue"
+            likeIcon.style.color = "black"   
+            commentContext.dislikeCommentByIds(props.comment.commentId, currentUser.userId)
+        }
+
+        // replace with amount of users that like/dislike the comment
+        const likeCount = 0
+        const dislikeCount = 0
+
+        likeNum.innerText = likeCount
+        dislikeNum.innerText = dislikeCount
+
+    }
+
 
     return (
         <div className="comment-container">
             {/** TODO when profiles are set up <Link to={"/profile/" + props.comment.userId}/> */}
 
-            {getProfilePicture()}
-            {/* <img className="comment-user" src={getProfilePicture()} alt={currentUser.userName}></img> */}
+            <img className="comment-user" src={getProfilePicture()} alt={currentUser.userName}></img>
 
             <div className="comment-text">{props.comment.text}</div>
             <div className="vote-section">
                 <div className="vote-container">
-                    <button className="vote-button" onClick={likeComment}> Like </button>
-                    <span className="vote-number"> NUMBER </span>
+                    <i className="fa fa-thumbs-up" id={"likeIcon" + props.comment.commentId} aria-hidden="true" onClick={likeComment}></i>
+                    <span className="vote-number" id={"likeNum" + props.comment.commentId}>0</span>
                 </div>
                 <div className="vote-container">
-                    <button className="vote-button" > Dislike </button>
-                    <span className="vote-number"> NUMBER </span>
+                    <i className="fa fa-thumbs-down" id={"dislikeIcon" + props.comment.commentId} aria-hidden="true" onClick={dislikeComment}></i>
+                    <span className="vote-number" id={"dislikeNum" + props.comment.commentId}>0</span>
                 </div>
             </div>
 
 
             <div className="comment-date">{props.comment.date}</div>
-            <button onClick={deleteComment} className="comment-delete">X</button>
-            {/* {
+            {
                     currentUser?.userId === props.comment.userId || currentUser?.isAdmin ?
                     <button onClick={deleteComment} className="comment-delete">X</button> : null
-            } */}
+            }
         </div>
     );
 }
